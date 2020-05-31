639a330eed3142a4382df3a05be144bce562ec65
HunterGreen <hunter.green95@hotmail.com>
2020-05-31 16:15:23 +0930

    Test class updated to match database simplification

--- app/src/test/java/com/huntergreen/recipemate/ExampleUnitTest.java
+++ app/src/test/java/com/huntergreen/recipemate/ExampleUnitTest.java
@@ -1,6 +1,5 @@
 package com.huntergreen.recipemate;
 
-import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 
@@ -11,7 +10,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
@@ -55,8 +53,8 @@ public class ExampleUnitTest {
 
     @Test
     public void writeRecipeAndReadInList() throws Exception{
-        Recipe bakedPotato = new Recipe(1,"Baked Potato", "60m","5m",5);
-        Ingredient potato = new Ingredient(1,"500g","potato");
+        Recipe bakedPotato = new Recipe(1,"Baked Potato",5);
+        Ingredient potato = new Ingredient("potato");
         Step s1 = new Step(1,"Wrap potato in alfoil then place in oven at 180 degrees C for 1 hour",1,1);
         Step s2 = new Step(2,"Let potato rest for 10 minutes and then enjoy!",2,1);
 
@@ -65,7 +63,6 @@ public class ExampleUnitTest {
         recipeDao.insertIngredient(potato);
         recipeDao.insertStep(s1);
         recipeDao.insertStep(s2);
-
 
 
         List<Recipe> recipes = recipeDao.getAllRecipes();
@@ -79,7 +76,7 @@ public class ExampleUnitTest {
         assertEquals(2,recipeDao.getRecipeSteps(testRecipeID).size());
 
         assertEquals(testRecipe.getName(), bakedPotato.getName());
-        assertEquals(ingredients.get(0).getName(), potato.getName());
+        assertEquals(ingredients.get(0).getIdentifier(), potato.getIdentifier());
         assertEquals(steps.get(0).getStep(),s1.getStep());
 
     }
