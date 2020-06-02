19c3e31fefe271d8b8ef8875391961b72befd2a2
HunterGreen <hunter.green95@hotmail.com>
2020-06-02 13:52:43 +0930

    RecipeDB class now contains a singleton

--- app/src/main/java/database/RecipeDB.java
+++ app/src/main/java/database/RecipeDB.java
@@ -1,12 +1,32 @@
 package database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(version = 1, entities = {Recipe.class, Step.class, Ingredient.class})
 public abstract class RecipeDB extends RoomDatabase {
 
     public abstract RecipeDao recipeDao();
+
+    private static volatile RecipeDB INSTANCE;
+
+    /*
+        Method derived from : https://www.youtube.com/watch?v=WquAAoBFBPU&ab_channel=yoursTRULY
+        Double null check is used to ensure multiple singletons are not initialised on multiple threads
+     */
+    static RecipeDB getInstance(Context context){
+        if(INSTANCE == null){
+            synchronized (RecipeDB.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),RecipeDB.class, "Recipe_Database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 
 
 }
