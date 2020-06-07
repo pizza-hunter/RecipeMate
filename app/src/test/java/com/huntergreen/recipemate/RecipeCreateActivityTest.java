febce7492d632969982166097c2e50e4c1f797d1
HunterGreen <hunter.green95@hotmail.com>
2020-06-07 12:35:56 +0930

    Test class for the recipe creation activity

---
+++ app/src/test/java/com/huntergreen/recipemate/RecipeCreateActivityTest.java
@@ -0,0 +1,13 @@
+package com.huntergreen.recipemate;
+
+import android.os.Build;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.O_MR1)
+public class RecipeCreateActivityTest {
+
+}
