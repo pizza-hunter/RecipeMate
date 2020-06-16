1b08f685a92c705bac2f91180908825f889c5a3e
HunterGreen <hunter.green95@hotmail.com>
2020-06-16 12:00:58 +0930

    Initialise ingredient and step arraylists

--- app/src/main/java/com/huntergreen/recipemate/RecipeListItemDetailActivity.java
+++ app/src/main/java/com/huntergreen/recipemate/RecipeListItemDetailActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 import database.Recipe;
 import databaseManager.DatabaseManager;
 
@@ -13,17 +15,23 @@ public class RecipeListItemDetailActivity extends AppCompatActivity {
     private DatabaseManager dbm;
     private Recipe recipe;
     private Intent intent;
+    private ArrayList<String> ingredientStrings;
+    private ArrayList<String> stepStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list_item_detail);
-        dbm = new DatabaseManager(getApplicationContext());
-        intent = getIntent();
-        recipe = dbm.getRecipe(intent.getStringExtra("name"));
+        this.dbm = new DatabaseManager(getApplicationContext());
+        this.intent = getIntent();
+        this.recipe = dbm.getRecipe(intent.getStringExtra("name"));
+        this.ingredientStrings = dbm.getRecipeIngredients(recipe);
+        this.stepStrings = dbm.getRecipeSteps(recipe);
     }
 
-    public void generateIngredients(){}
+    public void generateIngredients(){
+
+    }
     /*
         Get information about recipe through intent position var
         Create text views for each ingredient
